415d9294912b50327e315f9e0f0b1ac2aaff4321
CaptainBern <captainberneby@gmail.com>
2014-08-12 14:00:13 +0200

    created basic Main class

--- src/main/java/org/entityapi/test/Main.java
+++ src/main/java/org/entityapi/test/Main.java
@@ -1,16 +1,33 @@
 package org.entityapi.test;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.entityapi.api.EntityManager;
+import org.entityapi.api.plugin.EntityAPI;
 
 public class Main extends JavaPlugin {
 
+    private EntityManager entityManager;
+
     @Override
     public void onEnable() {
+
+        if (!Bukkit.getPluginManager().isPluginEnabled("EntityAPI")) {
+            getLogger().info("EntityAPI is not enabled! -> disabling");
+            Bukkit.getPluginManager().disablePlugin(this);
+            return;
+        }
+
+        this.entityManager = EntityAPI.createManager(this);
 
     }
 
     @Override
     public void onDisable() {
 
+    }
+
+    public EntityManager getEntityManager() {
+        return this.entityManager;
     }
 }
