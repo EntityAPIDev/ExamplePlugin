80b57b592fd31c89bb2425396d1cb25451aee8a5
CaptainBern <captainberneby@gmail.com>
2014-08-12 13:54:22 +0200

    yay

--- src/main/java/org/entityapi/test/Main.java
+++ src/main/java/org/entityapi/test/Main.java
@@ -1,4 +1,16 @@
 package org.entityapi.test;
 
-public class Main {
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
 }
